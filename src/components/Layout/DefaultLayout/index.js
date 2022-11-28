09e6f16afdbec10e37465d0717e5cc9535660d75
tinh <ngovantinh0698@gmail.com>
2022-11-28 16:15:58 +0700

    Xây dựng UI Header #1

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,20 @@
+import classNames from 'classnames/bind';
+import Styles from './DefaultLayout.module.scss';
+import Header from '~/components/Layout/components/Header';
+import Sidebar from './Sidebar';
+
+const cx = classNames.bind(Styles);
+
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
