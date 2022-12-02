107b38323c97b1599132ef0dc06c242ecc04bb35
tinh <ngovantinh0698@gmail.com>
2022-12-02 15:20:12 +0700

    Xây dựng UI phần sidebar #3

--- src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -15,7 +15,9 @@ function AccountPreview() {
                     src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1670126400&x-signature=nWb3PR%2Bz1a%2FhnxaNJnDKXuSfPSU%3D"
                     alt=""
                 />
-                <Button primary>Follow</Button>
+                <Button className={cx('follow-btn')} primary>
+                    Follow
+                </Button>
             </div>
             <div className={cx('body')}>
                 <p className={cx('nickname')}>
